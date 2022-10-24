73f86804218b0a67fc0f7171d93b1e8aaf6b9ff4
luis-casas <116565894+luis-casas@users.noreply.github.com>
2022-10-24 11:23:11 -0300

    incluindo console.log

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("Rodando o sitema de gerenciamento de jogos")
+console.log("Aplicação esta se conectando com o banco de dados")
+//mais um console de log de testes:
+console.log("O Casas fez esse commit sem autorização")
+console.log("Enviaando para o github")
